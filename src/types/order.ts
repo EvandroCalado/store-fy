2a9282bc4d763b545cf9273a3c434f7edb8931c2
Evandro Calado <evandrocalado07@gmail.com>
2025-04-14 08:57:26 -0300

    fix: extend Order type to include id property

--- src/types/order.ts
+++ src/types/order.ts
@@ -2,4 +2,6 @@ import { z } from 'zod';
 
 import { insertOrderSchema } from '@/schemas/insert-order';
 
-export type Order = z.infer<typeof insertOrderSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+};
