e74549fd173aa39cc9921da93e6fcbbabca3a4d3
Evandro Calado <evandrocalado07@gmail.com>
2025-04-15 21:26:25 -0300

    fix: add missing properties to Order type definition

--- src/types/order.ts
+++ src/types/order.ts
@@ -4,4 +4,9 @@ import { insertOrderSchema } from '@/schemas/insert-order';
 
 export type Order = z.infer<typeof insertOrderSchema> & {
   id: string;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  createdAt: Date;
 };
