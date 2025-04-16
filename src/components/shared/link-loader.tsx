aa6b56df669bbff1f7c96b9e9ba6a985d703618b
Evandro Calado <evandrocalado07@gmail.com>
2025-04-15 21:26:36 -0300

    fix: replace MoveLeft icon with Link icon in LinkLoader component

--- src/components/shared/link-loader.tsx
+++ src/components/shared/link-loader.tsx
@@ -2,12 +2,10 @@
 
 import { useLinkStatus } from 'next/link';
 
-import { Loader2, MoveLeft } from 'lucide-react';
+import { Link, Loader2 } from 'lucide-react';
 
 export const LinkLoader = () => {
   const { pending } = useLinkStatus();
 
-  return (
-    <div>{pending ? <Loader2 className='animate-spin' /> : <MoveLeft />}</div>
-  );
+  return <div>{pending ? <Loader2 className='animate-spin' /> : <Link />}</div>;
 };
