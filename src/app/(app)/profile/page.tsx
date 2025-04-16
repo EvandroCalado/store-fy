7c11751bb121bdc5074429e2fde0816b944c4e36
Evandro Calado <evandrocalado07@gmail.com>
2025-04-16 18:03:54 -0300

    feat: create ProfilePage component with session management and user information display

---
+++ src/app/(app)/profile/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+
+import { SessionProvider } from 'next-auth/react';
+
+import { auth } from '@/auth';
+import { ProfileForm } from '@/components/profile/profile-form';
+import { Container } from '@/components/shared/container';
+import { SectionTitle } from '@/components/shared/section-title';
+
+export const metadata: Metadata = {
+  title: 'Perfil',
+};
+
+const ProfilePage = async () => {
+  const session = await auth();
+
+  return (
+    <SessionProvider session={session}>
+      <SectionTitle title='Perfil' />
+      <Container className='my-8'>
+        <h2 className='text-xl font-semibold'>Informações do usuário</h2>
+        <ProfileForm />
+      </Container>
+    </SessionProvider>
+  );
+};
+
+export default ProfilePage;
