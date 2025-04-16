746471b9470627b170f0862fbcbcbfff40fa47be
Evandro Calado <evandrocalado07@gmail.com>
2025-04-16 20:52:38 -0300

    feat: restructure AdminHeader component layout for improved organization

--- src/components/admin/admin-header.tsx
+++ src/components/admin/admin-header.tsx
@@ -1,13 +1,17 @@
+import { DarkMode } from '../shared/dark-mode';
 import { Input } from '../ui/input';
 import { SidebarTrigger } from '../ui/sidebar';
 
 export const AdminHeader = () => {
   return (
     <header className='flex items-center justify-between gap-4 border-b p-4'>
-      <SidebarTrigger
-        title='Abir menu lateral'
-        aria-label='Abir menu lateral'
-      />
+      <div>
+        <SidebarTrigger
+          title='Abir menu lateral'
+          aria-label='Abir menu lateral'
+        />
+        <DarkMode />
+      </div>
 
       <Input
         type='search'
