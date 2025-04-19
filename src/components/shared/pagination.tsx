1cd5cebb1c32dd647f63d996386e7ae5b0a89558
Evandro Calado <evandrocalado07@gmail.com>
2025-04-18 21:06:20 -0300

    feat: add loading indicator to pagination buttons for improved user feedback

--- src/components/shared/pagination.tsx
+++ src/components/shared/pagination.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { useTransition } from 'react';
+
 import { useRouter, useSearchParams } from 'next/navigation';
 
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Loader } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import { formatUrlQuery } from '@/utils/format-url-query';
@@ -22,32 +24,35 @@ export const Pagination = ({
   urlParams,
   className,
 }: PaginationProps) => {
+  const [isPending, startTransition] = useTransition();
   const searchParams = useSearchParams();
   const router = useRouter();
 
   if (totalPages <= 1) return null;
 
   const handleClick = (type: 'prev' | 'next') => {
-    const pageValue = type === 'prev' ? page - 1 : page + 1;
+    startTransition(() => {
+      const pageValue = type === 'prev' ? page - 1 : page + 1;
 
-    const newUrl = formatUrlQuery({
-      params: searchParams.toString(),
-      key: urlParams || 'page',
-      value: pageValue.toString(),
+      const newUrl = formatUrlQuery({
+        params: searchParams.toString(),
+        key: urlParams || 'page',
+        value: pageValue.toString(),
+      });
+
+      router.push(newUrl);
     });
-
-    router.push(newUrl);
   };
 
   return (
     <div className={cn('flex items-center gap-2', className)}>
       <Button
-        size='icon'
         variant='outline'
-        disabled={page <= 1}
+        disabled={page <= 1 || isPending}
         onClick={() => handleClick('prev')}
       >
-        <ChevronLeft />
+        {isPending ? <Loader className='animate-spin' /> : <ChevronLeft />}
+        Anterior
       </Button>
 
       <div className='mx-2 text-sm'>
@@ -55,12 +60,12 @@ export const Pagination = ({
       </div>
 
       <Button
-        size='icon'
         variant='outline'
-        disabled={page >= totalPages}
+        disabled={page >= totalPages || isPending}
         onClick={() => handleClick('next')}
       >
-        <ChevronRight />
+        Próximo
+        {isPending ? <Loader className='animate-spin' /> : <ChevronRight />}
       </Button>
     </div>
   );
