b0955225aa1694c20b37145b1c16c669d205feee
Evandro Calado <evandrocalado07@gmail.com>
2025-04-19 23:29:46 -0300

    feat: implement createProduct function for product creation

---
+++ src/actions/create-product.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import { z } from 'zod';
+
+import { prisma } from '@/db/prisma';
+import { createProductSchema } from '@/schemas/create-product';
+import { formatErrors } from '@/utils/formatErrors';
+
+type CreateProduct = z.infer<typeof createProductSchema>;
+
+export const createProduct = async (data: CreateProduct) => {
+  try {
+    const product = createProductSchema.parse(data);
+
+    await prisma.product.create({
+      data: product,
+    });
+
+    revalidatePath('/admin/products');
+
+    return {
+      success: true,
+      message: 'Produto criado com sucesso',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatErrors(error),
+    };
+  }
+};
