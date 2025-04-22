b1b3d6f78fa9cc3fdec5dd655edcd8b254ce8653
Evandro Calado <evandro.calado.da.silva@gmail.com>
2025-04-21 23:38:26 -0300

    fix: update currency schema to use number type and refine validation logic

--- src/schemas/currency.ts
+++ src/schemas/currency.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 import { formatNumberWithDecimal } from '@/utils/formatNumberWithDecimal';
 
-export const currencySchema = z
-  .any()
+export const currencySchema = z.coerce
+  .number()
   .refine(
-    value => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+    value => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
     'Price must be exactly 2 decimal places',
   );
