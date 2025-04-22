de9d62a201ea2dab35922420f5790cb1a31db52b
Evandro Calado <evandro.calado.da.silva@gmail.com>
2025-04-21 21:10:19 -0300

    fix: change currency schema type from number to any

--- src/schemas/currency.ts
+++ src/schemas/currency.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 import { formatNumberWithDecimal } from '@/utils/formatNumberWithDecimal';
 
 export const currencySchema = z
-  .number()
+  .any()
   .refine(
     value => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
     'Price must be exactly 2 decimal places',
