a743e7ebeb5dc2bc36b576be375357ea6fc3106a
Evandro Calado <evandro.calado.da.silva@gmail.com>
2025-04-23 23:47:06 -0300

    feat: implement updateUser action for user data updates

---
+++ src/actions/update-user.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import { z } from 'zod';
+
+import { prisma } from '@/db/prisma';
+import { updateUserSchema } from '@/schemas/update-user';
+import { formatErrors } from '@/utils/formatErrors';
+
+type UpdateUserSchema = z.infer<typeof updateUserSchema>;
+
+export const updateUser = async (data: UpdateUserSchema) => {
+  try {
+    const user = updateUserSchema.parse(data);
+
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath('/admin/users');
+
+    return {
+      success: true,
+      message: 'Usuário atualizado com sucesso',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatErrors(error),
+    };
+  }
+};
