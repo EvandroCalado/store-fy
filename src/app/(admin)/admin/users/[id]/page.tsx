5296e6f85e129f951c57519be079c683b426a2ec
Evandro Calado <evandro.calado.da.silva@gmail.com>
2025-04-23 23:47:26 -0300

    feat: add AdminUserPage and AdminUsersUpdate component for user management

---
+++ src/app/(admin)/admin/users/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+import { getUserById } from '@/actions/get-user-by-id';
+import { AdminUsersUpdate } from '@/components/admin/admin-users-update';
+import { Container } from '@/components/shared/container';
+
+export const metadata: Metadata = {
+  title: 'Atualizar usuário',
+};
+
+type AdminUserPageParams = {
+  params: Promise<{ id: string }>;
+};
+
+const AdminUserPage = async ({ params }: AdminUserPageParams) => {
+  const { id } = await params;
+
+  const user = await getUserById(id);
+
+  if (!user) return notFound();
+
+  return (
+    <Container className='my-8'>
+      <AdminUsersUpdate user={user} />
+    </Container>
+  );
+};
+
+export default AdminUserPage;
---
+++ src/components/admin/admin-users-update.tsx
@@ -0,0 +1,144 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { User } from '@prisma/client';
+import { Loader2, SaveAll } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { z } from 'zod';
+
+import { updateUser } from '@/actions/update-user';
+import { updateUserSchema } from '@/schemas/update-user';
+import { CONSTANTS } from '@/utils/constants';
+
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+
+type UpdateUserSchema = z.infer<typeof updateUserSchema>;
+
+type AdminUsersUpdateProps = {
+  user: User;
+};
+
+export const AdminUsersUpdate = ({ user }: AdminUsersUpdateProps) => {
+  const router = useRouter();
+
+  const form = useForm<UpdateUserSchema>({
+    resolver: zodResolver(updateUserSchema),
+    defaultValues: user,
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (values: UpdateUserSchema) => {
+    const res = await updateUser(values);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    router.push('/admin/users');
+
+    toast.success(res.message);
+  };
+
+  return (
+    <div className='space-y-8'>
+      <h1 className='text-xl font-semibold'>Detalhe do Usuários</h1>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+          <div className='grid gap-5 md:grid-cols-2'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem className='relative'>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input type='text' placeholder='john Doe' {...field} />
+                  </FormControl>
+                  <FormMessage className='absolute -bottom-5 left-0' />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem className='relative'>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type='email'
+                      placeholder='example@email.com'
+                      disabled
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className='absolute -bottom-5 left-0' />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name='role'
+            render={({ field }) => (
+              <FormItem className='relative'>
+                <FormLabel>Cargo</FormLabel>
+                <FormControl>
+                  <Select value={field.value} onValueChange={field.onChange}>
+                    <SelectTrigger>
+                      <SelectValue placeholder='Selecione um cargo' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {CONSTANTS.USER_ROLES.map(role => (
+                        <SelectItem key={role} value={role}>
+                          {role === 'admin' ? 'Administrador' : 'Usuário'}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage className='absolute -bottom-5 left-0' />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type='submit'
+            className='my-2'
+            disabled={form.formState.isSubmitting}
+          >
+            Salvar
+            {form.formState.isSubmitting ? (
+              <Loader2 className='animate-spin' />
+            ) : (
+              <SaveAll />
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
