0b6d4a54b9ad74fb4730a5e26a548d0833260ae7
Evandro Calado <evandrocalado07@gmail.com>
2025-04-26 22:02:45 -0300

    feat: add section title SVG and update SectionTitle component for background image

--- src/components/shared/section-title.tsx
+++ src/components/shared/section-title.tsx
@@ -14,7 +14,7 @@ type SectionTitleProps = {
 
 export const SectionTitle = ({ title }: SectionTitleProps) => {
   return (
-    <div className='bg-muted flex h-40 w-full flex-col items-center justify-center overflow-hidden'>
+    <div className='flex h-40 w-full flex-col items-center justify-center overflow-hidden bg-[url("/bg/section-title.svg")]'>
       <Container className='relative space-y-4'>
         <Breadcrumb>
           <BreadcrumbList>
@@ -29,10 +29,6 @@ export const SectionTitle = ({ title }: SectionTitleProps) => {
         <h2 className='text-muted-foreground text-2xl font-semibold tracking-tight md:text-4xl'>
           {title}
         </h2>
-
-        <span className='text-muted-foreground/[5%] absolute right-4 -bottom-20 text-[85px] font-bold tracking-tighter sm:-bottom-32 sm:text-[180px] md:right-8 md:-bottom-36 md:text-[210px]'>
-          Store<span className='text-primary/[5%]'>FY</span>
-        </span>
       </Container>
     </div>
   );
