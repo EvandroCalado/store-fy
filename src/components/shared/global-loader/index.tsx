5e8ceb9ba1d6b8a49b11e5309a7f1bdb21d19ff6
Evandro Calado <evandrocalado07@gmail.com>
2025-04-27 22:53:06 -0300

    refactor: restructure GlobalLoader component for improved readability and maintainability

--- src/components/shared/global-loader/index.tsx
+++ src/components/shared/global-loader/index.tsx
@@ -10,19 +10,21 @@ export function GlobalLoader() {
   const { pending } = useLinkStatus();
 
   return (
-    <div
-      className={cn('fixed inset-0 -z-10 flex items-center justify-center', {
-        'bg-background/90 z-10': pending,
-      })}
-    >
+    <>
       {pending && (
-        <div className='text-foreground flex flex-col items-center gap-2'>
-          <LoaderIcon className='animate-spin' />
-          <p className='text-xl font-semibold'>
-            Store <span className='text-primary'>Fy</span>
-          </p>
+        <div
+          className={cn(
+            'bg-background/90 fixed inset-0 z-10 flex items-center justify-center',
+          )}
+        >
+          <div className='text-foreground flex flex-col items-center gap-2'>
+            <LoaderIcon className='animate-spin' />
+            <p className='text-xl font-semibold'>
+              Store <span className='text-primary'>Fy</span>
+            </p>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
