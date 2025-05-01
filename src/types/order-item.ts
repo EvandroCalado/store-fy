bfee4b4911c05ba8678cb1b9a000175ca79e8c4d
Evandro Calado <evandrocalado07@gmail.com>
2025-05-01 15:41:36 -0300

    refactor: remove commented-out properties from OrderItem type definition

--- src/types/order-item.ts
+++ src/types/order-item.ts
@@ -2,16 +2,4 @@ import { z } from 'zod';
 
 import { insertOrderItemSchema } from '@/schemas/insert-order-item';
 
-export type OrderItem = z.infer<typeof insertOrderItemSchema> & {
-  // id: string;
-  // isPaid: boolean;
-  // paidAt: Date | null;
-  // isDelivered: boolean;
-  // deliveredAt: Date | null;
-  // orderItem: OrderItem[];
-  // createdAt: Date;
-  // user: {
-  //   name: string;
-  //   email: string;
-  // };
-};
+export type OrderItem = z.infer<typeof insertOrderItemSchema> & {};
